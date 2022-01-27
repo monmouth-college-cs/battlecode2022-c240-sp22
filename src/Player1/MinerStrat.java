c9dfe53d37a9461c1d2879460090eb8d864ba821
WyattMayor <wmayor@monmouthcollege.edu>
2022-01-27 11:11:31 -0600

    second try

---
+++ src/Player1/MinerStrat.java
@@ -0,0 +1,68 @@
+package Player1;
+
+import battlecode.common.*;
+import java.util.Random;
+
+strictfp class MinerStrat {
+    static Direction exploreDir = null;
+    /**
+     * Run a single turn for a Miner.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runMiner(RobotController rc) throws GameActionException {
+        // gives each miner a specific id
+        if(exploreDir == null){
+            RobotPlayer.rng.setSeed(rc.getID());
+            exploreDir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        }
+        rc.setIndicatorString(exploreDir.toString());
+        MapLocation me = rc.getLocation();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                // Sense Led and looks for it. only mines it if led is greater then one so led can continue to respawn. as long as one led
+                // is left it will respawn
+                while (rc.canMineGold(mineLocation)) {
+                    rc.mineGold(mineLocation);
+                }
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
+                    rc.mineLead(mineLocation);
+                }
+            }
+        }
+        // This function gives the miner to be able to see in a radius and detect different materials.
+        int visionRadius = rc.getType().visionRadiusSquared;
+        MapLocation[] nearbyLocations = rc.getAllLocationsWithinRadiusSquared(me, visionRadius);
+
+        MapLocation targetLocation = null;
+        int distanceToTarget = Integer.MAX_VALUE;
+        for (MapLocation tryLocation : nearbyLocations) {
+            if (rc.senseLead(tryLocation) > 1 || rc.senseGold(tryLocation) > 0) {
+                int distanceTo = me.distanceSquaredTo(tryLocation);
+                if (distanceTo < distanceToTarget) {
+                    targetLocation = tryLocation;
+                    distanceToTarget = distanceTo;
+                }
+            }
+        }
+        // says if there isnt any lead then move on and continue to move.
+        if (targetLocation != null) {
+            Direction toMove = me.directionTo(targetLocation);
+            if (rc.canMove(toMove)) {
+                rc.move(toMove);
+            }
+        }   else {
+            if(rc.canMove(exploreDir)){
+                rc.move(exploreDir);
+            } else if (rc.onTheMap(rc.getLocation().add(exploreDir))){
+                exploreDir = exploreDir.opposite();
+        }
+        }
+        // Also try to move randomly.
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            System.out.println("I moved!");
+        }
+    }
+}
